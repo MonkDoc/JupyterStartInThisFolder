a3ff548df98afa243899ade0015c988928c22018
Arne <arnsen@gmx.de>
2020-01-17 11:55:06 +0100

    Windows soultion
    just copy the files in a folder run batch and jupyter will put home folder to the folder wehre the files are

---
+++ Jupyter_StartInThisFolder.bat
@@ -0,0 +1,2 @@
+C:\ProgramData\Anaconda3\python.exe cwp_mod.py C:\ProgramData\Anaconda3 %~dp0 C:\ProgramData\Anaconda3\python.exe C:\ProgramData\Anaconda3\Scripts\jupyter-notebook-script.py %~dp0
+::pause
